c2c86a65874c147850f139183f98eb3eb4f9ed14
Gilad Tsabar <gilad@tsabar.net>
2020-06-15 15:00:17 +0200

    restructure for production

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+
+var app = express();
+var environment = app.get('env');
+
+app.set('view engine', 'react');
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}));
+
+console.log(environment);
+
+
+if (environment === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  // The "catchall" handler: for any request that doesn't
+  // match one above, send back React's index.html file.
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  });
+}
+
+module.exports = app;
