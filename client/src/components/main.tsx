492fd8cd13bb86c80d4c4b2b9e1ce8c07006a8f2
Gilad Tsabar <gilad@tsabar.net>
2020-07-01 13:14:16 +0200

    Add query with hooks

--- client/src/components/main.tsx
+++ client/src/components/main.tsx
@@ -9,14 +9,24 @@ import RootPage from './Root/root_static';
 import styles from './main.module.css';
 import AdminRoot from './Root/Admin/AdminRoot';
 
-let Main = (params:any) => {
-    return(
-      <div className={styles.main}>
-        <Route exact path={`/`} component={RootPage} />
-        <Route exact path={`/:page/:user/:employer`} >
-          <Root google={params.google} routes={params.routes} />
-        </Route>
-        <Route path={`/admin`} component={AdminRoot} />
+import { useQuery } from '@apollo/react-hooks';
+import { GET_CANDIDATE_QL } from './../queries/queries';
+import Spinner from './Root/spinner';
+
+const Main = (params:any) => {
+  const { loading, error, data } = useQuery(GET_CANDIDATE_QL,{variables: {id:'5ebfa08e2793e32a4bdce817'}});
+  if(loading) return (<Spinner/>)
+  if(error) return <p>`Error! ${error.message}`</p>;
+
+  return(
+    <div className={styles.main}>
+      <Route exact path={`/`} component={RootPage} />
+      <Route exact path={`/:page/:user/:employer`} >
+        <Root google={params.google} routes={params.routes} />
+      </Route>
+      <Route path={`/admin`}>
+        <AdminRoot key='admin_root' data={data} />
+      </Route>
     </div>
   )
 }
