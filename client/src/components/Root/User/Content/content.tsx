b28e92b739dfa340413b5a730018f1a3124caabd
Gilad Tsabar <gilad@tsabar.net>
2020-08-16 22:07:23 +0200

    small cleanup

--- client/src/components/Root/User/Content/content.tsx
+++ client/src/components/Root/User/Content/content.tsx
@@ -1,4 +1,4 @@
-import React, { useState , useEffect } from 'react';
+import React, { useState } from 'react';
 import { Route } from "react-router-dom";
 import styles from './content.module.css';
 
